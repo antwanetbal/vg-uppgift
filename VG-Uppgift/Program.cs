7a4cde5e1bdd6a7f3f51755f8f1bb479dbde4c96
antwanetbal <antwanet.ballan.92@gmail.com>
2022-10-30 21:25:36 +0100

    firstcomment

---
+++ VG-Uppgift/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VG_Uppgift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("the vguppgift");
+        }
+    }
+}
